79d03fa5e2fb9249645dcfa6b4e2de5e217664f5
rezadrian01 <ahmadadrian324@gmail.com>
2024-11-16 20:06:30 +0700

    basic condition, and basic loop

--- 1. Introduction/hello-rust/src/main.rs
+++ 1. Introduction/hello-rust/src/main.rs
@@ -1,11 +1,28 @@
 
-use ferris_says::say;
-use std::io::{stdout, BufWriter};
+// use ferris_says::say;
+// use std::io::{stdout, BufWriter};
 fn main() {
-    let stdout = stdout();
-    let message = String::from("Hello fellow Rustaceans");
-    let width = message.chars().count();
+    // let stdout = stdout();
+    // let message = String::from("Hello fellow Rustaceans");
+    // let width = message.chars().count();
 
-    let mut writer = BufWriter::new(stdout.lock());
-    say(&message, width, &mut writer).unwrap();
+    // let mut writer = BufWriter::new(stdout.lock());
+    // say(&message, width, &mut writer).unwrap();
+    let num1 = 5;
+    let num2 = 10;
+    let sum = num1 + num2;
+    println!("{}", sum);
+    if 5 < 2 {
+        println!("True condition");
+    }else{
+        println!("False condition");
+    }
+    let mut counter = 0;
+    while counter < 5{
+        println!("{}", counter);
+        counter += 1;
+    }
+    for i in 0..10{
+        println!("{}", i);
+    }
 }
