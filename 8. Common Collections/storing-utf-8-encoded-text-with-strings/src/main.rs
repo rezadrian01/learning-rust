cdd12d9ebecaaeb196e162f1032ac6f3b5b5b90d
rezadrian01 <ahmadadrian324@gmail.com>
2024-11-22 19:49:26 +0700

    Storing UTF-8 Encoded Text with Strings

---
+++ 8. Common Collections/storing-utf-8-encoded-text-with-strings/src/main.rs
@@ -0,0 +1,46 @@
+fn main() {
+    // let mut s = String::new();
+
+    // let data = "initial contents";
+    // let s = data.to_string();
+    //
+    // // the method also works on a literal directly
+    // let s = "initial contents".to_string();
+    // println!("{s}");
+
+    // let mut s1 = String::from("foo");
+    // let s2 = "bar";
+    // s1.push_str(s2);
+    // println!("{s1}");
+
+    // let mut s = String::from("lo");
+    // // push method for push char to string
+    // s.push('l');
+    // println!("{s}");
+
+    // Concatenate string
+    // let s1 = String::from("Hello, ");
+    // let s2 = String::from("world");
+    // let s3 = s1 + &s2;
+    // println!("{s3}");
+
+    // let s1 = String::from("tic");
+    // let s2 = String::from("tac");
+    // let s3 = String::from("toe");
+    //
+    // let s = format!("{s1}-{s2}-{s3}");
+    // println!("{s}");
+    //
+    // // s1, s2, and s3 is still valid because format take a reference not ownership
+    // println!("{s1}-{s2}-{s3}");
+
+    // In string, we cant indexing a string
+
+    // for c in "hello".chars(){
+    //     println!("{c}");
+    // }
+
+    // for c in "hello".bytes(){
+    //     println!("{c}");
+    // }
+}
